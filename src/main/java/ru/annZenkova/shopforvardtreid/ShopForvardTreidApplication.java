22c834e38291f350d3187d7869acaf270013285c
AnZenkova <nastyonka16@mail.ru>
2022-09-12 20:31:59 +1000

    Initial commit

---
+++ src/main/java/ru/annZenkova/shopforvardtreid/ShopForvardTreidApplication.java
@@ -0,0 +1,13 @@
+package ru.annZenkova.shopforvardtreid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopForvardTreidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopForvardTreidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/annZenkova/shopforvardtreid/ShopForvardTreidApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.annZenkova.shopforvardtreid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopForvardTreidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
